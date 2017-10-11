74f3706ddbc9e5210b8affe77281a5f8e0fa14c2
Robert Fiorentino <rfiorentino@new-host-4.home>
2017-10-10 23:19:47 -0400

    Initial commit

---
+++ AnotherFramework/AnotherFramework.h
@@ -0,0 +1,19 @@
+//
+//  AnotherFramework.h
+//  AnotherFramework
+//
+//  Created by Robert Fiorentino on 10/10/17.
+//  Copyright © 2017 The Meet Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnotherFramework.
+FOUNDATION_EXPORT double AnotherFrameworkVersionNumber;
+
+//! Project version string for AnotherFramework.
+FOUNDATION_EXPORT const unsigned char AnotherFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnotherFramework/PublicHeader.h>
+
+
